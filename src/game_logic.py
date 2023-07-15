520419df0cfe30bbeb0163a77d8de358371a2b0d
nathan <nathanberry97@live.com>
2023-07-15 19:17:56 +0100

    adding score to snake game

--- src/game_logic.py
+++ src/game_logic.py
@@ -10,6 +10,18 @@ class game_logic:
         self.background = background
         self.snake = snake(self.screen)
         self.apple = apple(self.screen)
+        self.SCORE = {
+            "0": "zero",
+            "1": "one",
+            "2": "two",
+            "3": "three",
+            "4": "four",
+            "5": "five",
+            "6": "six",
+            "7": "seven",
+            "8": "eight",
+            "9": "nine",
+        }
 
     def main_loop(self, game_loop: bool) -> None:
         """The main loop of the game"""
@@ -60,16 +72,32 @@ class game_logic:
     def __display_score(self) -> None:
         """Method to display current score"""
 
-        sprite = "../assets/numbers/zero.png"
+        score_list = list(str(self.snake.get_score()))
 
-        self.__draw_sore(sprite, 215, 18)
-        self.__draw_sore(sprite, 240, 18)
-        self.__draw_sore(sprite, 265, 18)
+        digit_one = self.__get_digit(score_list, 0, 1)
+        digit_two = self.__get_digit(score_list, 1, 2)
+        digit_three = self.__get_digit(score_list, 2, 3)
+
+        self.__draw_sore(digit_one, 265, 18)
+        self.__draw_sore(digit_two, 240, 18)
+        self.__draw_sore(digit_three, 215, 18)
 
     def __draw_sore(self, sprite: str, x: int, y: int) -> None:
-        """Method to draw snake onto the display"""
+        """Method to draw score onto the display"""
 
-        import_sprite = pygame.image.load(sprite).convert_alpha()
+        import_sprite = pygame.image.load(
+            f"../assets/numbers/{sprite}.png"
+        ).convert_alpha()
         resize_sprite = pygame.transform.scale_by(import_sprite, 5)
 
         self.screen.blit(resize_sprite, (x, y))
+
+    def __get_digit(self, score: list[str], index: int, num: int) -> str:
+        """Method get the digit for the score"""
+
+        digit = "zero"
+
+        if len(score) > index:
+            digit = self.SCORE[score[-num]]
+
+        return digit
